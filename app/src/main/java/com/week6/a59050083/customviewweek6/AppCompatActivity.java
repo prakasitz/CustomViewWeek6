d24fcfe599d5fa76f7c1db522eefa01b4e77c1ff
prakasit <prakasitbm@gmail.com>
2018-09-22 13:51:08 +0700

    Initial commit

---
+++ app/src/main/java/com/week6/a59050083/customviewweek6/AppCompatActivity.java
@@ -0,0 +1,4 @@
+package com.week6.a59050083.customviewweek6;
+
+class AppCompatActivity {
+}
---
+++ app/src/main/java/com/week6/a59050083/customviewweek6/CustomView.java
@@ -0,0 +1,13 @@
+package com.week6.a59050083.customviewweek6;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CustomView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_view);
+    }
+}
